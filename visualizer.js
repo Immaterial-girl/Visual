621e5a8fc4e164e0da480b5d59252d2f0c1149cf
Bipp <1401980@cityplym.ac.uk>
2025-07-05 15:36:39 +0100

    Update visualizer.js

--- visualizer.js
+++ visualizer.js
@@ -1,3 +1,8 @@
+const barResolution = 2.5;
+const fftSize = 256;
+const barSpacing = 1;
+const barColorBase = [100, 50, 50];
+
 const audioFileInput = document.getElementById('audioFile');
 const canvas = document.getElementById('visualizer');
 const ctx = canvas.getContext('2d');
@@ -15,7 +20,7 @@ audioFileInput.addEventListener('change', function () {
       source.buffer = buffer;
 
       const analyser = audioContext.createAnalyser();
-      analyser.fftSize = 256;
+      analyser.fftSize = fftSize;
       const bufferLength = analyser.frequencyBinCount;
       const dataArray = new Uint8Array(bufferLength);
 
@@ -27,20 +32,21 @@ audioFileInput.addEventListener('change', function () {
         requestAnimationFrame(draw);
 
         analyser.getByteFrequencyData(dataArray);
-
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        const barWidth = (canvas.width / bufferLength) * 2.5;
-        let barHeight;
+        const barWidth = (canvas.width / bufferLength) * barResolution;
         let x = 0;
 
         for (let i = 0; i < bufferLength; i++) {
-          barHeight = dataArray[i];
+          const barHeight = dataArray[i];
+          const red = barHeight + barColorBase[0];
+          const green = barColorBase[1];
+          const blue = barColorBase[2];
 
-          ctx.fillStyle = 'rgb(' + (barHeight + 100) + ',50,50)';
+          ctx.fillStyle = `rgb(${red},${green},${blue})`;
           ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
 
-          x += barWidth + 1;
+          x += barWidth + barSpacing;
         }
       }
 
